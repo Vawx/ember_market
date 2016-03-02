a45c4a2aca85ec263bd5621372a10875a53107cc
James Nielson and Kyle Langley <altck8@gmail.com>
2016-03-02 11:38:26 -0800

    add service and helper for shopping cart

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  shoppingCart: Ember.inject.service(),
+
   model( ) {
     return this.store.findAll('inventory');
+  },
+  actions: {
+    addToCart(inventory){
+      this.get('shoppingCart').add(inventory);
+    }
   }
+
 });
